a46145de49e43c0bdcdca86de5413b189bda4855
PenguScripts <102926606+PenguScript@users.noreply.github.com>
2024-02-21 18:38:56 -0500

    v1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'pengu_shart'
+version '1.0.0'
+
+client_script {
+	'client/main.lua',
+}
+
+lua54 'yes'
+
+files {
+	'audio/data/audioexample_sounds.dat54.rel',
+	'audio/audiodirectory/pengu_farts.awc',
+}
+
+data_file 'AUDIO_WAVEPACK' 'audio/audiodirectory'
+data_file 'AUDIO_SOUNDDATA' 'audio/data/audioexample_sounds.dat'
