ad2c061bf1200064bf3c91af229486242d0b97e5
Donivan <donivansee@live.com>
2024-02-24 16:42:33 -0500

    add randomseed, moved order of lines, adjusted readme
    Random seed required me to change the way the random operator method worked; without making one static and one random, it would be the same every time. I moved dist/sound before the audio bank to prevent loading/getting coordinates if they weren't used.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 All config options are located in the `client.lua`.
 
-Discord for Support: [https://discord.gg/AS7U5GK4TE](https://discord.gg/AS7U5GK4TE)
+Discord for Support: [Discord](https://discord.gg/AS7U5GK4TE)
 
-Preview: [https://youtu.be/7_wDj9niMhs](https://youtu.be/7_wDj9niMhs)
+Preview: [Youtube](https://youtu.be/7_wDj9niMhs)
--- client/main.lua
+++ client/main.lua
@@ -1,20 +1,21 @@
 local audioCount, distMin, distMax = 9, 4.0, 8.0
 
 local function RollChanceToShart()
+    math.randomseed(GetGameTimer())
     local ran = math.random(2)
-    if ran == ran then return math.random(distMin, distMax), math.random(audioCount) end
+    if ran == 1 then return math.random(distMin, distMax), math.random(audioCount) end
     return false
 end
 
 AddEventHandler('entityDamaged', function(victim, culprit, weapon, damage)
     if weapon == -842959696 then
         local ped = PlayerPedId()
+        local dist, sound = RollChanceToShart()
+        if not dist then return end
         RequestScriptAudioBank('audiodirectory/pengu_farts', true)
         Wait(1)
         local id = GetSoundId()
         local coords = GetEntityCoords(ped)
-        local dist, sound = RollChanceToShart()
-        if not dist then return end
         Wait(500)
         PlaySoundFromCoord(id, tostring(sound), coords.x, coords.y, coords.z, 'special_soundset', 0, dist, 0)
         ReleaseSoundId(id)
